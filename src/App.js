5686a47d3fffd123d944bfb61cd9b2f1c3c85e8a
idankuch <idan.ku@gmail.com>
2022-12-20 14:24:20 +0200

    commit changes

--- src/App.js
+++ src/App.js
@@ -4,28 +4,45 @@ import CharactersList from "./components/CharacterList";
 import "./App.css";
 
 function App() {
-  console.log("App rendered")
-  const [apiDB, setApiDB] = useState([]); 
-  const [lastShownItem, setLastShownItem] = useState(4); 
-  const [isDisabled, setIsDisabled ] = useState(false);
-  
+  console.log("App rendered");
+  const [apiDB, setApiDB] = useState();
+  const [lastShownItem, setLastShownItem] = useState(4);
+  const [isDisabled, setDisabled] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  useEffect( ()=>{
+  useEffect(() => {
     fetch("https://thronesapi.com/api/v2/Characters")
-    .then(res => res.json())
-    .then(data => {setApiDB(data); console.log(apiDB)});
-  } ,[]);
-  
-const onShowMoreClick = () => {
-  setLastShownItem(lastShownItem + 4);
-  lastShownItem >= apiDB.length ? setIsDisabled(true) : null;
-}
+      .then((res) => res.json())
+      .then((data) => {
+        setApiDB(data);
+        setLoading(false);
+      });
+  }, []);
 
-  return <div className="container">
-    <CharactersList characters={apiDB.slice(0, lastShownItem)}/>
-    <Button className="btn" onClick={onShowMoreClick} isDisabled={isDisabled}>More</Button>
+  const onShowMoreClick = () => {
+    setLastShownItem(lastShownItem + 4);
+    lastShownItem >= apiDB.length ? setDisabled(true) : setDisabled(false);
+  };
+
+  return (
+    <div className='container'>
+      {isLoading && !error ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <CharactersList characters={apiDB.slice(0, lastShownItem)} />
+          <Button
+            className='btn'
+            onClick={onShowMoreClick}
+            isDisabled={isDisabled}
+          >
+            More
+          </Button>
+        </>
+      )}
     </div>
-  
+  );
 }
 
 export default App;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,22 +1,21 @@
 import React from "react";
 import Character from "./Character";
 
-function CharactersList (props){
-     
-if (props.characters){
+function CharactersList(props) {
+  if (props.characters) {
     return (
-            <div className="grid">
-                {
-                props.characters.map(character => 
-                    <Character key={character.id} 
-                        firstname={character.firstName} 
-                        lastname={character.lastName} 
-                        imageSrc={character.imageUrl}/>
-                        )
-                }
-            </div>
-        )
-    }
+      <div className='grid'>
+        {props.characters.map((character) => (
+          <Character
+            key={character.id}
+            firstname={character.firstName}
+            lastname={character.lastName}
+            imageSrc={character.imageUrl}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default CharactersList;
